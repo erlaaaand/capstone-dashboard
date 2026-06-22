7cf804df2c579897ac38b4413b0568e5fa7d3d04
erlaaaand <erlandagsya2@gmail.com>
2026-06-22 09:35:24 +0700

    update core

---
+++ src/core/constants/app.constants.ts
@@ -0,0 +1,11 @@
+export const APP_CONFIG = {
+  NAME: 'Durian Classifier Admin',
+  DEFAULT_PAGE_LIMIT: 10,
+};
+
+export const DURIAN_VARIETIES = [
+  { code: 'D197', name: 'Musang King' },
+  { code: 'D24', name: 'Sultan' },
+  { code: 'D13', name: 'Golden Bun' },
+  { code: 'D2', name: 'Dato Nina' },
+];
